dbb3e84a76312b2d97f4be5094be1cfb257ee90b
LucaVerth <luca.verthuy@gmail.com>
2021-12-14 17:16:39 +0100

    Carousel section done // working on other section

--- src/components/OnlineCourses.vue
+++ src/components/OnlineCourses.vue
@@ -9,21 +9,29 @@
         <div class="col-12">
           <div class="carousel">
             <div class="main-carousel">
-              <div class="cards" v-for="(card, index) in CardsToPrint" :key="index">
+              <div
+                class="cards"
+                v-for="(card, index) in CardsToPrint"
+                :key="index"
+              >
                 <a class="course-image" href="#">
-                  <img class="rounded-top" :src="require(`../assets/images/${card.image}`)"  alt="" />
+                  <img
+                    class="rounded-top"
+                    :src="require(`../assets/images/${card.image}`)"
+                    alt=""
+                  />
                 </a>
                 <div class="text">
-                  <span>${{card.price}}</span>
+                  <span>${{ card.price }}</span>
                   <h4 class="mt-3"></h4>
                   <div class="extra-info mt-4">
                     <span class="me-3">
                       <i class="far fa-file-alt"></i>
-                      {{card.lessons}} Lessons
+                      {{ card.lessons }} Lessons
                     </span>
                     <span class="ms-3">
                       <i class="far fa-user"></i>
-                      {{card.students}} Students
+                      {{ card.students }} Students
                     </span>
                   </div>
                 </div>
@@ -38,7 +46,12 @@
             </div>
           </div>
         </div>
-        <div class="col-12 text-center">TEXT AND LINK END SECTION</div>
+        <div class="col-6 offset-3 text-center courses-link mt-4">
+          <p>
+            Control your personal preferences settings to get notified about
+            appropriate courses. <a href="#">View all courses &#8594;</a>
+          </p>
+        </div>
       </div>
     </div>
   </section>
@@ -49,17 +62,17 @@ import CardsToPrint from "../assets/data/Cards.js";
 
 export default {
   name: "OnlineCourses",
-  data(){
+  data() {
     return {
       CardsToPrint,
-    }
+    };
   },
 };
 </script>
 
 <style lang="scss" scoped>
-.courses{
-  h2{
+.courses {
+  h2 {
     font-size: 50px;
     margin-top: 20px;
     margin-bottom: 60px;
@@ -77,8 +90,8 @@ export default {
     }
   }
 }
-.carousel-dots{
-  button{
+.carousel-dots {
+  button {
     background-color: #171520;
     opacity: 0.3;
     border-radius: 100%;
@@ -86,12 +99,17 @@ export default {
     height: 10px;
     border: none;
     margin: 15px;
-    &.active{
+    &.active {
       width: 15px;
       height: 15px;
       background-color: #171520;
       opacity: 1;
     }
+  }
+}
+.courses-link {
+  p {
+    font-size: 1.2rem;
   }
 }
 </style>
