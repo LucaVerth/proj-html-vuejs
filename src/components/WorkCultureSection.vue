22417b25ba510c5ee9952c26b03ed2fbddbcecb1
LucaVerth <luca.verthuy@gmail.com>
2021-12-14 12:48:28 +0100

    Work culture section done // working on main sections

--- src/components/WorkCultureSection.vue
+++ src/components/WorkCultureSection.vue
@@ -4,22 +4,12 @@
       <div class="row">
         <div class="col-12 top-half">
           <div class="text-side">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit
-              repudiandae sunt nostrum, magnam repellendus quam animi, quasi
-              quae illum sit neque placeat voluptatibus unde nemo quis alias?
-              Perferendis, vero harum. Lorem ipsum dolor, sit amet consectetur
-              adipisicing elit. Quae, vero. Ipsa obcaecati, dolor necessitatibus
-              quibusdam nemo aut dolorem consequatur eaque reprehenderit esse
-              nisi natus aspernatur, sint magnam nulla sed facilis? Lorem ipsum
-              dolor sit amet consectetur adipisicing elit. Beatae, molestiae
-              optio inventore magni nulla illum dolor error sint placeat.
-              Debitis minus nesciunt id maiores laborum ratione ullam aliquam.
-              Consectetur, eius. Lorem ipsum dolor sit amet consectetur
-              adipisicing elit. Nostrum repellat natus vero enim amet sequi
-              iusto corporis, delectus ratione, architecto quo deserunt omnis
-              explicabo, totam aliquam ducimus voluptatum dolor nemo?
-            </p>
+            <div>
+              <span class="slogan text-uppercase">everything in maxcoach</span>
+              <h2 class="my-3 ">Learn about our <span class="fw-light">Work Culture</span> at MaxCoach</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi dolor tempore odit mollitia ducimus exercitationem illo voluptates repudiandae, ea, explicabo cupiditate! Dolore, officia corporis laudantium maiores impedit eum ipsa. Eligendi.</p>
+              <span>Have questions? <a href="#">Get Free Guide &#8594;</a> </span>
+            </div>
           </div>
           <div class="image-mosaic">
             <img class="image1" :src="require('../assets/images/stock-full-hd-10-ohki1ayywq3cqutettrncduujpoaorifqu4relt2ts.jpg')" alt="" />
@@ -29,7 +19,17 @@
             
           </div>
         </div>
-        <div class="col-12 bottom-half">BOTTOM SIDE</div>
+        <div class="col-12 bottom-half">
+          <div class="stats">129+
+            <span>ENROLLED LEARNERS</span>
+          </div>
+          <div class="stats">80
+            <span>FINISHED SESSIONS</span>
+          </div>
+          <div class="stats">4%
+            <span>SATISFACTION RATE</span>
+          </div>
+        </div>
       </div>
     </div>
   </section>
@@ -42,20 +42,22 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-section {
-  background-color: red;
-}
 .top-half {
   display: flex;
-  margin-top: 50px;
+  margin-top: 80px;
+  margin-bottom: 60px;
   .text-side {
+    display: flex;
+    justify-content:start;
+    align-items: center;
     width: 40%;
-    background-color: green;
     min-height: 500px;
+    h2{
+      font-size: 50px;
+    }
   }
   .image-mosaic {
     width: 60%;
-    background-color: purple;
     position: relative;
     .image1 {
       position: absolute;
@@ -74,7 +76,7 @@ section {
     }
     .image3{
       position: absolute;
-      top: 0;
+      top: -10px;
       right: 110px;
       border-radius: 5px;
       z-index: 1;
@@ -85,6 +87,24 @@ section {
       right: 20%;
       border-radius: 5px;
       z-index: 4;
+    }
+  }
+
+}
+.bottom-half{
+  display: flex;
+  justify-content: space-evenly;
+  text-align: center;
+  .stats{
+    display: flex;
+    flex-direction: column;
+    margin: 0 -100px;
+    font-size: 2.3rem;
+    font-weight: bold;
+    span{
+      padding-top: 10px;
+      font-size: 1rem;
+      font-weight: normal;
     }
   }
 }
