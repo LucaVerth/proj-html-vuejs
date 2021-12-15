305642ad1356ef36eded3d5f0722b68d7cfd77f4
LucaVerth <luca.verthuy@gmail.com>
2021-12-15 09:42:58 +0100

    Footer work in progress // address add

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,33 @@
   <footer>
     <div class="container">
       <div class="row">
-        <div class="col-12"></div>
+        <div class="col-12 text-light footer_custom">
+          <div class="address">
+            <h5 class="fw-bold">Address</h5>
+            <div>
+              <ul class="address-list">
+                <li>382 NE 191st St # 87394 Miami, FL 33179-3899</li>
+                <li>+1 (305) 547-9909 (9am - 5pm EST, Monday - Friday)</li>
+                <li>support@maxcoach.com</li>
+              </ul>
+            </div>
+            <div class="social-links">
+              <a href="#">
+                <i class="fab fa-facebook-square"></i>
+              </a>
+              <a href="#">
+                <i class="fab fa-twitter"></i>
+              </a>
+              <a href="#">
+                <i class="fab fa-instagram"></i>
+              </a>
+              <a href="#">
+                <i class="fab fa-linkedin"></i>
+              </a>
+            </div>
+          </div>
+          <div class="links">LINKS</div>
+        </div>
       </div>
     </div>
   </footer>
@@ -10,13 +36,44 @@
 
 <script>
 export default {
-  name: 'Footer',
-}
+  name: "Footer",
+};
 </script>
 
 <style lang="scss" scoped>
-footer{
+footer {
   height: 550px;
   background-color: #171520;
+}
+.footer_custom {
+  display: flex;
+  justify-content: space-between;
+  .address {
+    width: 50%;
+    padding: 70px 0;
+  }
+  .address ul {
+    list-style-type: none;
+    padding: 10px 0;
+    & li {
+      padding: 10px 0;
+    }
+  }
+  .social-links {
+    width: 35%;
+    display: flex;
+    justify-content: space-between;
+    & i {
+      font-size: 1.8rem;
+    }
+    a{
+      text-decoration: none;
+      color: #fff;
+    }
+  }
+  .links {
+    width: 50%;
+    padding: 70px 0;
+  }
 }
 </style>
